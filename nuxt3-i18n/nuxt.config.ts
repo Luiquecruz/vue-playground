ffafa07bfc5f5bc3b12e1bddd58ef3af169085c6
luiquecruz <luiquecruz@gmail.com>
2022-07-31 19:38:01 -0300

    Initial commit

--- nuxt3-i18n/nuxt.config.ts
+++ nuxt3-i18n/nuxt.config.ts
@@ -1,6 +1,18 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode'
+  ],
+  colorMode: {
+    classSuffix: ''
+  },
+  tailwindcss: {
+    cssPath: '~/assets/css/tailwind.css',
+    configPath: 'tailwind.config.js',
+    exposeConfig: false,
+    injectPosition: 0,
+    viewer: true,
+  },
 })
