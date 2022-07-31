20d080913742750f9147d05689c817018d744bd4
luiquecruz <luiquecruz@gmail.com>
2022-07-31 19:39:48 -0300

    chore: add default layout

---
+++ nuxt3-fullstack/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header />
+
+  <Alert message="Alert message" />
+  <Alert alert-type="success" message="Success! It works" />
+  <Alert alert-type="warning" message="Warning! It can be better" />
+  <Alert alert-type="error" message="Baaaannm! Error, it is wrong" />
+  <Alert alert-type="info" message="Did you know Vue is awesome?" />
+
+  <main>
+    <slot />
+  </main>
+</template>
