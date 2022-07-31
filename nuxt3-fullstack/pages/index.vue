9f961efc09e9014e0510eb019e8a385a0e7c2e03
luiquecruz <luiquecruz@gmail.com>
2022-07-31 19:40:45 -0300

    chore: pinia test

---
+++ nuxt3-fullstack/pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>{{ piniaTest.valueToCheck }}</h1>
+</template>
+
+<script setup lang="ts">
+import { usePiniaCheck } from "~/store/piniaCheck";
+
+const piniaTest = usePiniaCheck();
+</script>
